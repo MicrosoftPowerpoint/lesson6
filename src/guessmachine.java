49c0d815451c25a67c2220a4a1e68472c528cbfc
dani3700 <dani3700@ISS147D8-12.scdsb.pri>
2014-10-06 09:13:43 -0400

    started guessing game Oct 6

---
+++ src/guessmachine.java
@@ -0,0 +1,15 @@
+
+public class guessmachine {
+ private int number;   
+private int numGuesses;
+private int guess;
+
+public guessmachine(){
+    number = (int)(Math.random()*100)+1;
+    guess=0;
+    numGuesses= 0;
+    
+}
+//add methods
+// giveHint(), setGuess(), getNumGuesses()
+}
